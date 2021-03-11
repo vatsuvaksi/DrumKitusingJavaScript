1132fe979fbfd00f1ee303da332ffe0514d57d1d
vatsuvaksi <62776100+vatsuvaksi@users.noreply.github.com>
2021-03-11 22:58:22 +0530

    Added Styling and sound1

---
+++ index.js
@@ -0,0 +1,9 @@
+
+
+
+for(var i = 0; i< document.querySelectorAll(".drum").length ;i++){
+  document.querySelectorAll("button")[i].addEventListener("click",function(){
+    var audio = new Audio("sounds/tom-"+1+".mp3");
+    audio.play();
+  });
+}
